fab858152fd3fc94eb41b14b6bb0d462384a517c
Carolline Martins Victor <carolline.mvictor@dinfor.uniceub>
2025-08-20 21:42:29 -0300

    introdução ao thread

---
+++ CarollineMartins/Aulas/conhecendo-threads/src/TelaCalculadora.java
@@ -0,0 +1,32 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class TelaCalculadora {
+
+    public static void criar () {
+        JFrame janela = new JFrame("Multiplicação longa");
+
+        JTextField primeiro = new JTextField(18);
+        JTextField segundo = new JTextField(18);
+
+        JButton botao = new JButton(" = ");
+        JLabel resultado = new JLabel("Aguardando...");
+
+        botao.addActionListener(new AcaoBotao(primeiro, segundo, resultado));
+
+        JPanel painel = new JPanel();
+        painel.add(primeiro);
+        painel.add(new JLabel("X"));
+        painel.add(botao);
+        painel.add(segundo);
+        painel.add(resultado);
+
+        janela.add(painel);
+        janela.setVisible(true);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
